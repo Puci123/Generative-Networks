f7619b6e54e1af7bcb19f64923232430c1e84ab7
AdamS2002 <127023176+AdamS200@users.noreply.github.com>
2023-12-14 21:45:35 +0100

    Update main1.py - littel changes

--- main1.py
+++ main1.py
@@ -17,17 +17,14 @@ from torchvision import datasets, transforms
 
 import matplotlib.pyplot as plt
 
-# Przygotowanie transformacji danych
 transform = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize((0.5,), (0.5,))
 ])
 
-# Ładowanie danych
 train_dataset = datasets.FashionMNIST(root='./data', train=True, download=True, transform=transform)
 test_dataset = datasets.FashionMNIST(root='./data', train=False, download=True, transform=transform)
 
-# Tworzenie obiektów DataLoader
 batch_size = 100
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
